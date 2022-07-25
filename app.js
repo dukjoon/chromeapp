4c66d53155017b34716bea92455757e00109af4b
dukjoon <jungdukjoon@gmail.com>
2022-07-25 21:23:18 +0900

    #2.3

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 const a = 5;
 const b = 2;
 const veryLongVariableName = "Nico";
+const myName = "dukjoon";
 
 console.log(a + b);
 console.log(a * b);
 console.log(a / b);
-console.log("hello" + veryLongVariableName);
+console.log("hello" + myName);
+
+myName = "jungdukjoon"; //const로 값을 정하면 업데이트 할 수 없다.
+
+console.log("your new name is" + myName);
